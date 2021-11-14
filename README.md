b8861f342e82298d990036822f09227d214fe092
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2021-11-14 22:46:46 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+#Dodge Cars Server Side
+
+#In this project, we make a full stack website about Dodge Cars client side. Dodge is an American brand of automobiles and a division of Stellantis, based in Auburn Hills, Michigan. Dodge vehicles have historically included performance cars, and for much of its existence Dodge was Chrysler's mid-priced brand above Plymouth.
+
+#Five Important Points about this project:
+
+-> In Server Side, we use latest technology.
+-> We use mongodb.
+-> We use dotenv for secure server side.
+-> We use heroku for server side.
+-> We use cors for link client and server side.
+
+#Live Link:
---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e9cyj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run(){
+    try{
+        await client.connect();
+        const database = client.db('dodgeCar');
+        const itemsCollection = database.collection('items');
+
+         // GET API
+        app.get('/items', async (req,res)=>{
+            const cursor = itemsCollection.find({});
+            const offers = await cursor.toArray();
+            res.send(offers);
+        } );
+
+        // GET API
+
+        app.get('/items/:id', async (req,res)=>{
+            const id = req.params.id;
+            console.log('id', id);
+            const query = {_id: ObjectId(id)};
+            const offer = await itemsCollection.findOne(query);
+            res.json(offer);
+        })
+
+        // POST API
+        app.post('/items', async (req, res)=>{
+            const offer = req.body;
+            console.log('hit post',offer);
+
+            const result = await itemsCollection.insertOne(offer);
+            res.json(result);
+        })
+    }
+    finally{
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+     res.send('hello world');
+})
+
+app.listen(port, ()=> {
+    console.log(`example app listening on port ${port}`);
+})
